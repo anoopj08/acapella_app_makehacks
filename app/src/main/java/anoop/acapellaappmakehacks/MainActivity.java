1d33a81300074514b0a62556a700c98a9bc3742f
poonerwagon <anoopj08@gmail.com>
2015-11-14 14:46:17 -0800

    Initial commit

---
+++ app/src/main/java/anoop/acapellaappmakehacks/MainActivity.java
@@ -0,0 +1,112 @@
+package anoop.acapellaappmakehacks;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int VIDEO_CAPTURE = 101;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        File dir = new File( Environment.getExternalStorageDirectory()+"/media/acapella");
+        dir.mkdirs();
+
+        try{
+            if(dir.mkdir()) {
+                System.out.println("Directory created");
+            } else {
+                System.out.println("Directory is not created");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        if(hasCamera())
+        {
+            File mediaFile =
+                    new File(Environment.getExternalStorageDirectory()+"/media/acapella/myvideo.mp4");
+            try {
+                FileOutputStream fos = new FileOutputStream(mediaFile);
+            }
+            catch(FileNotFoundException e)
+            {
+                e.printStackTrace();
+            }
+
+
+            Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+            Uri videoUri = Uri.fromFile(mediaFile);
+
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, videoUri);
+            startActivityForResult(intent, VIDEO_CAPTURE);
+        }
+        else{
+            Toast.makeText(this, "You don't have a camera. Rekt.", Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VIDEO_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Video saved to:\n" +
+                        data.getData(), Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Video recording cancelled.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Failed to record video",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
+
+    private boolean hasCamera() {
+        if (getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA_FRONT)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
